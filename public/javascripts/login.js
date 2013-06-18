78fbd2b123fe22a7ebec5f4c1579ec1b3bd8183a
kui.wfly <kui.wfly@gmail.com>
2013-06-18 17:49:06 +0800

    jquery

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -6,19 +6,23 @@
 	
 	$(document).ready(function(){
 		$("#submit").click(function(){
+			//username:$("#username").val("hello") ;
+
+			//alert(username:$("#username").value) ;
+			//socket.emit('login',msg) ;
 			connect() ;
-			var msg = {
-			           username:$("#username").value,
-			           password:$("#password").value,
-			 			} ;
-			
+			var msg = {"username":$("#username").val(),
+			           "password":$("#password").val()
+			           } ;
 			socket.emit('login',msg) ;
+			socket.send('helo world') ;
+			alert($("#username").val()) ;
 		}) ;
 		
 	});	
 	
 	function connect(){
-		socket = io.connect("http://localhost:3000") ;
+		socket = io.connect("http://localhost:3000/") ;
 		socket.on('error',function(){
 				console.error('Unable to connect Socket.IO', reason);
 			}
@@ -45,7 +49,8 @@
 	    socket.on('reconnect_failed', function(){
 	    	
 	    	}
-	    );		
+	    );	
+	    socket.send('hello connect') ;
 	}
 	
 });
